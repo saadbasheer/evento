658dc235e2c7f943ca7d71ed22cbf39c943eacaf
Saadbasheer <saadbasheer0@gmail.com>
2024-05-01 00:24:46 +0530

    ..

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,7 +14,7 @@ const routes = [
 export default function Footer() {
   return (
     <footer className="mt-auto flex items-center justify-between h-16 border-t border-white/10 px-3 sm:px-9 text-xs text-white/25">
-      <small className="text-xs"> <Link href="github.com/saadbasheer/evento" > <span className="underline">saadbasheer</span></Link> portfolio project.</small>
+      <small className="text-xs"> <Link href="https://github.com/saadbasheer/evento" > <span className="underline">saadbasheer</span></Link> portfolio project.</small>
 
       <ul className="flex gap-x-3 sm:gap-x-8">
         {routes.map((route) => (
